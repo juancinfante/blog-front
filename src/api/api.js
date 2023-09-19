c0d9dee3192fcdb5a648e1e4dd427624e6ee65ab
juancinfante <juaninfantejj@gmail.com>
2023-09-19 09:40:43 -0300

    first2

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://blog-back-3lwg.onrender.com/',
+    baseURL: 'https://blog-back-mu.vercel.app/',
 })
 
 api.interceptors.request.use((config) => {
